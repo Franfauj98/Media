a80a939944b7d0c1c70b6a52da74bf7731daf5a3
franfauj98 <francois.faujeau@gmail.com>
2025-06-10 16:39:46 +0200

    upgrade angular

--- src/app/about/container/about/about.component.ts
+++ src/app/about/container/about/about.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-about',
-  standalone: true,
-  imports: [],
-  templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+    selector: 'app-about',
+    imports: [],
+    standalone: true,
+    templateUrl: './about.component.html',
+    styleUrl: './about.component.scss'
 })
 export class AboutComponent {
 
